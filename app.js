b8645525e1c1cb932f8ca6872d1627656c1ebea4
dindar123 <dr.deenmohd@gmail.com>
2017-01-13 10:20:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
 var vkApi = require('./vkApi');
-
-//Lets require/import the HTTP module
-var http = require('http');
-
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: https://chernika.herokuapp.com/:%s");
-});
