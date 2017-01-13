8ff20f187d70cffb5a61fe51a43f802b00568ee6
dindar123 <dr.deenmohd@gmail.com>
2017-01-13 10:49:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 var vkApi = require('./vkApi');
+
+//Lets require/import the HTTP module
+var http = require('http');
+
+//Lets define a port we want to listen to
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: https://lifeline519c.azurewebsites.net/");
+})
